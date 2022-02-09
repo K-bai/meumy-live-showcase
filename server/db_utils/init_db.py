95f9eee5570fe9ed1d4c6dd4897832b9ccb0bd40
K-bai <kongbaiyo@live.cn>
2022-02-09 16:36:23 +0800

    add init database script

---
+++ server/db_utils/init_db.py
@@ -0,0 +1,12 @@
+from db_declaration import Danmu, LiveStatus, Gift, SuperChat, Captain, View
+from peewee import SqliteDatabase
+
+path = "../db/8792912.db"
+#path = "db/22384516.db"
+
+db = SqliteDatabase(path, pragmas=(
+            ('cache_size', -1024 * 64),  # 64MB page-cache.
+            ('journal_mode', 'wal'))) # Use WAL-mode (you should always use this!).
+db.connect()
+db.bind([Danmu, LiveStatus, Gift, SuperChat, Captain, View])
+db.create_tables([Danmu, LiveStatus, Gift, SuperChat, Captain, View])
